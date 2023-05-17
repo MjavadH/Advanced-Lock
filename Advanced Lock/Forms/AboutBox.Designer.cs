4d90fe9e6a9534e7ccba7fd887097c44aa6054f2
M_javad Hacker <m.hacker0110@gmail.com>
2023-05-17 13:50:38 +0330

    Redesing 'AboutBox'

--- Advanced Lock/Forms/AboutBox.Designer.cs
+++ Advanced Lock/Forms/AboutBox.Designer.cs
@@ -29,160 +29,149 @@ namespace Advanced_Lock
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox));
-            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.logoPictureBox = new System.Windows.Forms.PictureBox();
-            this.labelProductName = new System.Windows.Forms.Label();
-            this.labelVersion = new System.Windows.Forms.Label();
-            this.labelCopyright = new System.Windows.Forms.Label();
-            this.labelCompanyName = new System.Windows.Forms.Label();
-            this.textBoxDescription = new System.Windows.Forms.TextBox();
-            this.okButton = new System.Windows.Forms.Button();
-            this.tableLayoutPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.label_Title = new System.Windows.Forms.Label();
+            this.label_Version = new System.Windows.Forms.Label();
+            this.label_Made = new System.Windows.Forms.Label();
+            this.panel_Title = new Guna.UI2.WinForms.Guna2Panel();
+            this.image_AppIcon = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.panel_Bottom = new Guna.UI2.WinForms.Guna2Panel();
+            this.label_inIran = new System.Windows.Forms.Label();
+            this.label_Heart = new System.Windows.Forms.Label();
+            this.button_OK = new Guna.UI2.WinForms.Guna2Button();
+            this.label_E_and_D = new System.Windows.Forms.Label();
+            this.label_Description = new System.Windows.Forms.Label();
+            this.linkLabel_Github = new System.Windows.Forms.LinkLabel();
+            this.panel_Title.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.image_AppIcon)).BeginInit();
+            this.panel_Bottom.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel
+            // label_Title
             // 
-            this.tableLayoutPanel.ColumnCount = 2;
-            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
-            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67F));
-            this.tableLayoutPanel.Controls.Add(this.logoPictureBox, 0, 0);
-            this.tableLayoutPanel.Controls.Add(this.labelProductName, 1, 0);
-            this.tableLayoutPanel.Controls.Add(this.labelVersion, 1, 1);
-            this.tableLayoutPanel.Controls.Add(this.labelCopyright, 1, 2);
-            this.tableLayoutPanel.Controls.Add(this.labelCompanyName, 1, 3);
-            this.tableLayoutPanel.Controls.Add(this.textBoxDescription, 1, 4);
-            this.tableLayoutPanel.Controls.Add(this.okButton, 1, 5);
-            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel.Location = new System.Drawing.Point(9, 9);
-            this.tableLayoutPanel.Name = "tableLayoutPanel";
-            this.tableLayoutPanel.RowCount = 6;
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(417, 265);
-            this.tableLayoutPanel.TabIndex = 0;
+            resources.ApplyResources(this.label_Title, "label_Title");
+            this.label_Title.Name = "label_Title";
             // 
-            // logoPictureBox
+            // label_Version
             // 
-            this.logoPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.logoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.Image")));
-            this.logoPictureBox.Location = new System.Drawing.Point(3, 3);
-            this.logoPictureBox.Name = "logoPictureBox";
-            this.tableLayoutPanel.SetRowSpan(this.logoPictureBox, 6);
-            this.logoPictureBox.Size = new System.Drawing.Size(131, 259);
-            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.logoPictureBox.TabIndex = 12;
-            this.logoPictureBox.TabStop = false;
+            resources.ApplyResources(this.label_Version, "label_Version");
+            this.label_Version.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label_Version.Name = "label_Version";
             // 
-            // labelProductName
+            // label_Made
             // 
-            this.labelProductName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelProductName.Location = new System.Drawing.Point(143, 0);
-            this.labelProductName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.labelProductName.MaximumSize = new System.Drawing.Size(0, 17);
-            this.labelProductName.Name = "labelProductName";
-            this.labelProductName.Size = new System.Drawing.Size(271, 17);
-            this.labelProductName.TabIndex = 19;
-            this.labelProductName.Text = "Advanced Lock";
-            this.labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            resources.ApplyResources(this.label_Made, "label_Made");
+            this.label_Made.Name = "label_Made";
             // 
-            // labelVersion
+            // panel_Title
             // 
-            this.labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelVersion.Location = new System.Drawing.Point(143, 26);
-            this.labelVersion.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.labelVersion.MaximumSize = new System.Drawing.Size(0, 17);
-            this.labelVersion.Name = "labelVersion";
-            this.labelVersion.Size = new System.Drawing.Size(271, 17);
-            this.labelVersion.TabIndex = 0;
-            this.labelVersion.Text = "1.1.0";
-            this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            resources.ApplyResources(this.panel_Title, "panel_Title");
+            this.panel_Title.BackColor = System.Drawing.Color.Transparent;
+            this.panel_Title.BorderRadius = 10;
+            this.panel_Title.Controls.Add(this.label_Version);
+            this.panel_Title.Controls.Add(this.label_Title);
+            this.panel_Title.FillColor = System.Drawing.Color.White;
+            this.panel_Title.Name = "panel_Title";
+            this.panel_Title.ShadowDecoration.Parent = this.panel_Title;
             // 
-            // labelCopyright
+            // image_AppIcon
             // 
-            this.labelCopyright.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelCopyright.Location = new System.Drawing.Point(143, 52);
-            this.labelCopyright.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.labelCopyright.MaximumSize = new System.Drawing.Size(0, 17);
-            this.labelCopyright.Name = "labelCopyright";
-            this.labelCopyright.Size = new System.Drawing.Size(271, 17);
-            this.labelCopyright.TabIndex = 21;
-            this.labelCopyright.Text = "Copyright 2022";
-            this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            resources.ApplyResources(this.image_AppIcon, "image_AppIcon");
+            this.image_AppIcon.Name = "image_AppIcon";
+            this.image_AppIcon.ShadowDecoration.Parent = this.image_AppIcon;
+            this.image_AppIcon.TabStop = false;
             // 
-            // labelCompanyName
+            // panel_Bottom
             // 
-            this.labelCompanyName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelCompanyName.Location = new System.Drawing.Point(143, 78);
-            this.labelCompanyName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.labelCompanyName.MaximumSize = new System.Drawing.Size(0, 17);
-            this.labelCompanyName.Name = "labelCompanyName";
-            this.labelCompanyName.Size = new System.Drawing.Size(271, 17);
-            this.labelCompanyName.TabIndex = 22;
-            this.labelCompanyName.Text = "Designer && Devloper : MjavadH";
-            this.labelCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            resources.ApplyResources(this.panel_Bottom, "panel_Bottom");
+            this.panel_Bottom.BackColor = System.Drawing.Color.Transparent;
+            this.panel_Bottom.BorderRadius = 10;
+            this.panel_Bottom.Controls.Add(this.label_inIran);
+            this.panel_Bottom.Controls.Add(this.label_Heart);
+            this.panel_Bottom.Controls.Add(this.button_OK);
+            this.panel_Bottom.Controls.Add(this.label_Made);
+            this.panel_Bottom.FillColor = System.Drawing.Color.White;
+            this.panel_Bottom.Name = "panel_Bottom";
+            this.panel_Bottom.ShadowDecoration.Parent = this.panel_Bottom;
             // 
-            // textBoxDescription
+            // label_inIran
             // 
-            this.textBoxDescription.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBoxDescription.Location = new System.Drawing.Point(143, 107);
-            this.textBoxDescription.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
-            this.textBoxDescription.Multiline = true;
-            this.textBoxDescription.Name = "textBoxDescription";
-            this.textBoxDescription.ReadOnly = true;
-            this.textBoxDescription.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxDescription.Size = new System.Drawing.Size(271, 126);
-            this.textBoxDescription.TabIndex = 23;
-            this.textBoxDescription.TabStop = false;
-            this.textBoxDescription.Text = "Description";
+            resources.ApplyResources(this.label_inIran, "label_inIran");
+            this.label_inIran.Name = "label_inIran";
             // 
-            // okButton
+            // label_Heart
             // 
-            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.okButton.Location = new System.Drawing.Point(339, 239);
-            this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 23);
-            this.okButton.TabIndex = 24;
-            this.okButton.Text = "&OK";
+            resources.ApplyResources(this.label_Heart, "label_Heart");
+            this.label_Heart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(185)))), ((int)(((byte)(0)))));
+            this.label_Heart.Name = "label_Heart";
+            // 
+            // button_OK
+            // 
+            resources.ApplyResources(this.button_OK, "button_OK");
+            this.button_OK.BorderRadius = 5;
+            this.button_OK.CheckedState.Parent = this.button_OK;
+            this.button_OK.CustomImages.Parent = this.button_OK;
+            this.button_OK.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button_OK.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(185)))), ((int)(((byte)(0)))));
+            this.button_OK.ForeColor = System.Drawing.Color.White;
+            this.button_OK.HoverState.Parent = this.button_OK;
+            this.button_OK.Name = "button_OK";
+            this.button_OK.ShadowDecoration.Parent = this.button_OK;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // label_E_and_D
+            // 
+            resources.ApplyResources(this.label_E_and_D, "label_E_and_D");
+            this.label_E_and_D.Name = "label_E_and_D";
+            // 
+            // label_Description
+            // 
+            resources.ApplyResources(this.label_Description, "label_Description");
+            this.label_Description.Name = "label_Description";
+            // 
+            // linkLabel_Github
+            // 
+            resources.ApplyResources(this.linkLabel_Github, "linkLabel_Github");
+            this.linkLabel_Github.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel_Github.Name = "linkLabel_Github";
+            this.linkLabel_Github.TabStop = true;
+            this.linkLabel_Github.VisitedLinkColor = System.Drawing.Color.MidnightBlue;
+            this.linkLabel_Github.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_Github_LinkClicked);
             // 
             // AboutBox
             // 
-            this.AcceptButton = this.okButton;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(435, 283);
-            this.Controls.Add(this.tableLayoutPanel);
-            this.Font = new System.Drawing.Font("Vazir", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Controls.Add(this.linkLabel_Github);
+            this.Controls.Add(this.label_Description);
+            this.Controls.Add(this.label_E_and_D);
+            this.Controls.Add(this.image_AppIcon);
+            this.Controls.Add(this.panel_Bottom);
+            this.Controls.Add(this.panel_Title);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutBox";
-            this.Padding = new System.Windows.Forms.Padding(9);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "AboutBox";
-            this.tableLayoutPanel.ResumeLayout(false);
-            this.tableLayoutPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.panel_Title.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.image_AppIcon)).EndInit();
+            this.panel_Bottom.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
-        private System.Windows.Forms.PictureBox logoPictureBox;
-        private System.Windows.Forms.Label labelProductName;
-        private System.Windows.Forms.Label labelVersion;
-        private System.Windows.Forms.Label labelCopyright;
-        private System.Windows.Forms.Label labelCompanyName;
-        private System.Windows.Forms.TextBox textBoxDescription;
-        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Label label_Title;
+        private System.Windows.Forms.Label label_Version;
+        private System.Windows.Forms.Label label_Made;
+        private Guna.UI2.WinForms.Guna2Panel panel_Title;
+        private Guna.UI2.WinForms.Guna2PictureBox image_AppIcon;
+        private Guna.UI2.WinForms.Guna2Panel panel_Bottom;
+        private Guna.UI2.WinForms.Guna2Button button_OK;
+        private System.Windows.Forms.Label label_inIran;
+        private System.Windows.Forms.Label label_Heart;
+        private System.Windows.Forms.Label label_E_and_D;
+        private System.Windows.Forms.Label label_Description;
+        private System.Windows.Forms.LinkLabel linkLabel_Github;
     }
 }
--- Advanced Lock/Forms/AboutBox.cs
+++ Advanced Lock/Forms/AboutBox.cs
@@ -14,92 +14,16 @@ namespace Advanced_Lock
         public AboutBox()
         {
             InitializeComponent();
-            this.Text = String.Format("About {0}", AssemblyTitle);
-            this.labelProductName.Text = AssemblyProduct;
-            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
-            this.labelCopyright.Text = AssemblyCopyright;
-            this.labelCompanyName.Text = AssemblyCompany;
-            this.textBoxDescription.Text = AssemblyDescription;
+            label_Version.Text = Assembly.GetExecutingAssembly().GetName().Version.ToString();
         }
-
-        #region Assembly Attribute Accessors
-
-        public string AssemblyTitle
+        private void button_OK_Click(object sender, EventArgs e)
         {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
-                if (attributes.Length > 0)
-                {
-                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
-                    if (titleAttribute.Title != "")
-                    {
-                        return titleAttribute.Title;
-                    }
-                }
-                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
-            }
+            this.Close();
         }
 
-        public string AssemblyVersion
+        private void linkLabel_Github_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            get
-            {
-                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            }
+            System.Diagnostics.Process.Start(linkLabel_Github.Text);
         }
-
-        public string AssemblyDescription
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
-            }
-        }
-
-        public string AssemblyProduct
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyProductAttribute)attributes[0]).Product;
-            }
-        }
-
-        public string AssemblyCopyright
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
-            }
-        }
-
-        public string AssemblyCompany
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCompanyAttribute)attributes[0]).Company;
-            }
-        }
-        #endregion
     }
 }
