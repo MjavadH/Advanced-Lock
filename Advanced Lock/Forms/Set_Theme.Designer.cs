6d30ccdee1e181ac45e0d4bb0ef52fa0ccd58eeb
M_javad Hacker <m.hacker0110@gmail.com>
2023-07-05 00:03:23 +0330

    fix bug 'Set_Theme'

--- Advanced Lock/Forms/Set_Theme.Designer.cs
+++ Advanced Lock/Forms/Set_Theme.Designer.cs
@@ -30,44 +30,46 @@ namespace Advanced_Lock
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Set_Theme));
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label_DarkMode = new System.Windows.Forms.Label();
+            this.label_LightMode = new System.Windows.Forms.Label();
+            this.label_Header = new System.Windows.Forms.Label();
             this.DarkMode = new Guna.UI2.WinForms.Guna2PictureBox();
             this.LightMode = new Guna.UI2.WinForms.Guna2PictureBox();
             this.panel_Title = new Guna.UI2.WinForms.Guna2Panel();
-            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.panel_LightMode = new Guna.UI2.WinForms.Guna2Panel();
+            this.panel_DarkMode = new Guna.UI2.WinForms.Guna2Panel();
             ((System.ComponentModel.ISupportInitialize)(this.DarkMode)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.LightMode)).BeginInit();
             this.panel_Title.SuspendLayout();
-            this.guna2Panel1.SuspendLayout();
-            this.guna2Panel2.SuspendLayout();
+            this.panel_LightMode.SuspendLayout();
+            this.panel_DarkMode.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // label_DarkMode
             // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
-            this.label1.Click += new System.EventHandler(this.DarkModeBTN_Click);
+            resources.ApplyResources(this.label_DarkMode, "label_DarkMode");
+            this.label_DarkMode.BackColor = System.Drawing.Color.Transparent;
+            this.label_DarkMode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(222)))), ((int)(((byte)(222)))));
+            this.label_DarkMode.Name = "label_DarkMode";
+            this.label_DarkMode.Click += new System.EventHandler(this.DarkModeBTN_Click);
             // 
-            // label2
+            // label_LightMode
             // 
-            resources.ApplyResources(this.label2, "label2");
-            this.label2.Name = "label2";
-            this.label2.Click += new System.EventHandler(this.LightMode_BTN_Click);
+            resources.ApplyResources(this.label_LightMode, "label_LightMode");
+            this.label_LightMode.Name = "label_LightMode";
+            this.label_LightMode.Click += new System.EventHandler(this.LightMode_BTN_Click);
             // 
-            // label3
+            // label_Header
             // 
-            resources.ApplyResources(this.label3, "label3");
-            this.label3.Name = "label3";
+            resources.ApplyResources(this.label_Header, "label_Header");
+            this.label_Header.Name = "label_Header";
             // 
             // DarkMode
             // 
+            resources.ApplyResources(this.DarkMode, "DarkMode");
             this.DarkMode.BackColor = System.Drawing.Color.Transparent;
             this.DarkMode.BorderRadius = 5;
             this.DarkMode.Image = global::Advanced_Lock.Properties.Resources.Dark_Screen;
-            resources.ApplyResources(this.DarkMode, "DarkMode");
             this.DarkMode.Name = "DarkMode";
             this.DarkMode.ShadowDecoration.Parent = this.DarkMode;
             this.DarkMode.TabStop = false;
@@ -75,9 +77,9 @@ namespace Advanced_Lock
             // 
             // LightMode
             // 
+            resources.ApplyResources(this.LightMode, "LightMode");
             this.LightMode.BorderRadius = 5;
             this.LightMode.Image = global::Advanced_Lock.Properties.Resources.Light_Screen;
-            resources.ApplyResources(this.LightMode, "LightMode");
             this.LightMode.Name = "LightMode";
             this.LightMode.ShadowDecoration.Parent = this.LightMode;
             this.LightMode.TabStop = false;
@@ -85,41 +87,41 @@ namespace Advanced_Lock
             // 
             // panel_Title
             // 
+            resources.ApplyResources(this.panel_Title, "panel_Title");
             this.panel_Title.BackColor = System.Drawing.Color.Transparent;
             this.panel_Title.BorderRadius = 15;
-            this.panel_Title.Controls.Add(this.label3);
+            this.panel_Title.Controls.Add(this.label_Header);
             this.panel_Title.FillColor = System.Drawing.Color.White;
-            resources.ApplyResources(this.panel_Title, "panel_Title");
             this.panel_Title.Name = "panel_Title";
             this.panel_Title.ShadowDecoration.Parent = this.panel_Title;
             // 
-            // guna2Panel1
+            // panel_LightMode
             // 
-            this.guna2Panel1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2Panel1.BorderRadius = 15;
-            this.guna2Panel1.Controls.Add(this.label2);
-            this.guna2Panel1.FillColor = System.Drawing.Color.White;
-            resources.ApplyResources(this.guna2Panel1, "guna2Panel1");
-            this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
+            resources.ApplyResources(this.panel_LightMode, "panel_LightMode");
+            this.panel_LightMode.BackColor = System.Drawing.Color.Transparent;
+            this.panel_LightMode.BorderRadius = 15;
+            this.panel_LightMode.Controls.Add(this.label_LightMode);
+            this.panel_LightMode.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(192)))), ((int)(((byte)(7)))));
+            this.panel_LightMode.Name = "panel_LightMode";
+            this.panel_LightMode.ShadowDecoration.Parent = this.panel_LightMode;
             // 
-            // guna2Panel2
+            // panel_DarkMode
             // 
-            this.guna2Panel2.BackColor = System.Drawing.Color.Transparent;
-            this.guna2Panel2.BorderRadius = 15;
-            this.guna2Panel2.Controls.Add(this.label1);
-            this.guna2Panel2.FillColor = System.Drawing.Color.White;
-            resources.ApplyResources(this.guna2Panel2, "guna2Panel2");
-            this.guna2Panel2.Name = "guna2Panel2";
-            this.guna2Panel2.ShadowDecoration.Parent = this.guna2Panel2;
+            resources.ApplyResources(this.panel_DarkMode, "panel_DarkMode");
+            this.panel_DarkMode.BackColor = System.Drawing.Color.Transparent;
+            this.panel_DarkMode.BorderRadius = 15;
+            this.panel_DarkMode.Controls.Add(this.label_DarkMode);
+            this.panel_DarkMode.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(17)))));
+            this.panel_DarkMode.Name = "panel_DarkMode";
+            this.panel_DarkMode.ShadowDecoration.Parent = this.panel_DarkMode;
             // 
             // Set_Theme
             // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
-            resources.ApplyResources(this, "$this");
-            this.Controls.Add(this.guna2Panel2);
-            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.panel_DarkMode);
+            this.Controls.Add(this.panel_LightMode);
             this.Controls.Add(this.panel_Title);
             this.Controls.Add(this.LightMode);
             this.Controls.Add(this.DarkMode);
@@ -131,20 +133,20 @@ namespace Advanced_Lock
             ((System.ComponentModel.ISupportInitialize)(this.DarkMode)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.LightMode)).EndInit();
             this.panel_Title.ResumeLayout(false);
-            this.guna2Panel1.ResumeLayout(false);
-            this.guna2Panel2.ResumeLayout(false);
+            this.panel_LightMode.ResumeLayout(false);
+            this.panel_DarkMode.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_DarkMode;
+        private System.Windows.Forms.Label label_LightMode;
+        private System.Windows.Forms.Label label_Header;
         private Guna.UI2.WinForms.Guna2PictureBox DarkMode;
         private Guna.UI2.WinForms.Guna2PictureBox LightMode;
         private Guna.UI2.WinForms.Guna2Panel panel_Title;
-        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
-        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2Panel panel_LightMode;
+        private Guna.UI2.WinForms.Guna2Panel panel_DarkMode;
     }
 }
