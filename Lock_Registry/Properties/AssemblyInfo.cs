5a6e8874b2d7efb7317dd2fa3c0c8225f29f60e0
M_javad Hacker <m.hacker0110@gmail.com>
2023-07-13 00:08:48 +0330

    Rename 'Lock_Registry' to RegistryEditor

--- Lock_Registry/Properties/AssemblyInfo.cs
+++ Lock_Registry/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lock_Registry")]
+[assembly: AssemblyTitle("RegistryEditor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lock_Registry")]
+[assembly: AssemblyCompany("MjavadH")]
+[assembly: AssemblyProduct("RegistryEditor")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
